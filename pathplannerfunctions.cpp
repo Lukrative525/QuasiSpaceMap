951381192f0dc886238be53880b735fd42ec9bea
Lukrative525 <lukegardner525@gmail.com>
2025-04-27 01:47:50 -0600

    implement floor int sqrt

--- pathplannerfunctions.cpp
+++ pathplannerfunctions.cpp
@@ -1,5 +1,3 @@
-#include <cmath>
-
 #include "pathplannerfunctions.h"
 
 namespace ppf
@@ -25,7 +23,7 @@ namespace ppf
 
     int calculateHypotenuse(int value_1, int value_2)
     {
-        int hypotenuse = std::sqrt(value_1 * value_1 + value_2 * value_2);
+        int hypotenuse = sqrt(value_1 * value_1 + value_2 * value_2);
 
         return hypotenuse;
     }
@@ -60,5 +58,31 @@ namespace ppf
         QString formatted_int = QString::asprintf("%d.%d", whole_part, decimal_part);
 
         return formatted_int;
+    }
+
+    int sqrt(int value)
+    {
+        int result = 0;
+        int highest_power_of_four = 1 << 30;
+
+        while (highest_power_of_four > value)
+            highest_power_of_four >>= 2;
+
+        while (highest_power_of_four != 0)
+        {
+            if (value >= result + highest_power_of_four)
+            {
+                value -= result + highest_power_of_four;
+                result = (result >> 1) + highest_power_of_four;
+            }
+            else
+            {
+                result >>= 1;
+            }
+
+            highest_power_of_four >>= 2;
+        }
+
+        return result;
     }
 }
--- pathplannerfunctions.h
+++ pathplannerfunctions.h
@@ -11,6 +11,7 @@ std::vector<int> calculateDifferenceBetween(int value, const std::vector<int>& v
 int calculateHypotenuse(int value_1, int value_2);
 std::vector<int> calculateHypotenuses(const std::vector<int>& vector_1, const std::vector<int>& vector_2);
 QString formatIntAsString(int number);
+int sqrt(int value);
 }
 
 #endif // PATHPLANNERFUNCTIONS_H
